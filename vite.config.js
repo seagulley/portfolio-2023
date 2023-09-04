cb57dfa437cbc08095e01a4d30db9f208742be55
seagulley <87551908+seagulley@users.noreply.github.com>
2023-09-04 15:49:30 -0400

    deploying

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio-2023/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/seagulley.github.io/' : '/',
   build: {
     outDir: 'docs'
   },
